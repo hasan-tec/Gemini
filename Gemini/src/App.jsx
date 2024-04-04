27a1eac678109cf3ed6189e19d36fa5731383cae
hasan-tec <131611130+hasan-tec@users.noreply.github.com>
2024-04-04 20:56:02 +0500

    first commit

---
+++ Gemini/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from "./components/Sidebar/sidebar"
+import Main from "./components/main/main"
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+
+  )
+}
+
+export default App
